969a8113547ac2c8895bfc2fe3e03a534b07dbc1
jinusong <sugwer7464@gmail.com>
2019-09-09 23:19:13 +0900

    [Update] execute-deploy sh

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/ubuntu/I-am-Full-Stack/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+sh /home/ubuntu/I-am-Full-Stack/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
